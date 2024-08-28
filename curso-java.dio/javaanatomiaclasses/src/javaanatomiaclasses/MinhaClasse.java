1fccc943454c28a4e948a67120579c9ba39add6f
Lucas5653 <lucas.augusto04@souunit.com.br>
2024-08-28 19:53:26 -0300

    Add files via upload

---
+++ curso-java.dio/javaanatomiaclasses/src/javaanatomiaclasses/MinhaClasse.java
@@ -0,0 +1,19 @@
+package javaanatomiaclasses;
+
+public class MinhaClasse {
+	public static void main(String[] args) {
+		String primeiroNome = "Lucas";
+		String segundoNome = "Andrade";
+		
+		String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+		System.out.println(nomeCompleto);
+
+	}
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+	return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+	
+}
+
+
+
+}
