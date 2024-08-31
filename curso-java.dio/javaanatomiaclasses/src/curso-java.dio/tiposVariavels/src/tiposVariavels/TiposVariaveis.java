8ffe92df69a9e3a470368160dacfbf171b1098b7
Lucas5653 <lucas.augusto04@souunit.com.br>
2024-08-31 17:55:44 -0300

    Add files via upload

---
+++ curso-java.dio/javaanatomiaclasses/src/curso-java.dio/tiposVariavels/src/tiposVariavels/TiposVariaveis.java
@@ -0,0 +1,21 @@
+package tiposVariavels;
+
+public class TiposVariaveis {
+	public static void main(String[] args) {
+		double salarioMinimo = 2500.33;
+		short numeroCurto = 1;
+		int numeroNormal = numeroCurto;
+		short numeroCurto2 = (short) numeroNormal;
+		
+		int numero =1;
+		
+		numero = 2;
+		System.out.println(numero);
+		
+		final double VALOR_DE_PI = 3.14;
+		
+		
+		
+	}
+
+}
